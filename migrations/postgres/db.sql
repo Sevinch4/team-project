9132f019b58bd6d814ee7841624f85b25046d3bd
Sevinch4 <nsevinch3@gmail.com>
2024-01-26 15:33:56 +0500

    db modified

--- migrations/postgres/db.sql
+++ migrations/postgres/db.sql
@@ -34,7 +34,32 @@ create table transactions(
     deleted_at       TIMESTAMP DEFAULT null
 );
 
+CREATE TABLE staff_tarifs (
+    id UUID PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL,
+    tarif_type tarif_type_enum NOT NULL,
+    amount_for_cash INT,
+    amount_for_card INT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    deleted_at TIMESTAMP DEFAULT NULL
+);
 
+CREATE TABLE staffs (
+        id UUID PRIMARY KEY,
+        branch_id UUID REFERENCES branches(id),
+        tariff_id UUID REFERENCES staff_tarifs(id),
+        staff_type staff_type_enum NOT NULL,
+        name VARCHAR(30),
+        balance INT DEFAULT 0,
+        age INT,
+        birth_date DATE,
+        login VARCHAR(15),
+        password VARCHAR(100),
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW(),
+        deleted_at TIMESTAMP DEFAULT NULL
+);
 create payment_type_enum as enum ('card', 'cash')
 
 create status_enum as enum ('in_process', 'success', 'cancel')
@@ -43,3 +68,6 @@ create transaction_type_enum as enum ('withdraw', 'topup')
 
 create source_type_enum('bonus', 'sales')
 
+CREATE TYPE tarif_type_enum AS ENUM ('percent', 'fixed');
+
+CREATE TYPE staff_type_enum AS ENUM ('shop_assistant', 'cashier');
